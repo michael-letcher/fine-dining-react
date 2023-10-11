2369687ab0e746c854993885a35360510f3562f8
Michael Letcher <mtrletcher@hotmail.com>
2023-10-11 14:35:29 +1100

    feat: menu section

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,5 +1,20 @@
 import './MenuItem.scss';
 
-const MenuItem = () => <div>MenuItem</div>;
+interface MenuItemProps {
+  title: string;
+  price: string;
+  tags: string;
+}
+
+const MenuItem = ({ title, price, tags }: MenuItemProps) => (
+  <div className="app_menuItem">
+    <div className="app_menuItem-title-wrapper">
+      <div className="app_menuItem-title">{title}</div>
+      <div className="app_menuItem-separator"></div>
+      <div className="app_menuItem-price">{price}</div>
+    </div>
+    <div className="app_menuItem-tags">{tags}</div>
+  </div>
+);
 
 export default MenuItem;
--- src/container/Menu/SpecialMenu.tsx
+++ src/container/Menu/SpecialMenu.tsx
@@ -1,5 +1,52 @@
+import { MenuItem, SubHeading } from '../../components';
+import { data, images } from '../../constants';
 import './SpecialMenu.scss';
 
-const SpecialMenu = () => <div>SpecialMenu</div>;
+const SpecialMenu = () => (
+  <section id="menu" className="app_specialMenu">
+    <div className="app_specialMenu-title">
+      <SubHeading title="Menu that fits your palate"></SubHeading>
+      <h1 className="headtext__cormorant">Today's Specials</h1>
+    </div>
+
+    <div className="app_specialMenu-menu">
+      <div className="app_specialMenu-menu_wine">
+        <p className="app_specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app_specialMenu-menu_items">
+          {data.wines.map((wine, index) => (
+            <MenuItem
+              key={wine.title + index}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            ></MenuItem>
+          ))}
+        </div>
+      </div>
+      <div className="app_specialMenu-menu_img">
+        <img src={images.menu} alt="menu" />
+      </div>
+      <div className="app_specialMenu-menu_cocktail">
+        <p className="app_specialMenu-menu_heading">Cocktails</p>
+        <div className="app_specialMenu-menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem
+              key={cocktail.title + index}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            ></MenuItem>
+          ))}
+        </div>
+      </div>
+    </div>
+
+    <div className="app_specialMenu-actions">
+      <button type="button" className="custom__button">
+        View Menu
+      </button>
+    </div>
+  </section>
+);
 
 export default SpecialMenu;
