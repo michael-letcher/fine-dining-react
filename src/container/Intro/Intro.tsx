2128c83fcf725d38072533856c7c3b9bb40590b4
Michael Letcher <mtrletcher@hotmail.com>
2023-10-11 15:06:20 +1100

    feat: intro section

--- src/container/Intro/Intro.tsx
+++ src/container/Intro/Intro.tsx
@@ -1,5 +1,43 @@
+import React, { useState } from 'react';
+import { BsPauseFill, BsPlayFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.scss';
 
-const Intro = () => <div>Intro</div>;
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = React.useRef<HTMLVideoElement>();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current?.pause();
+    } else {
+      vidRef.current?.play();
+    }
+  };
+
+  return (
+    <section className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      ></video>
+      <div className="app__video-overlay">
+        <div className="app__video-overlay_circle" onClick={handleVideo}>
+          {playVideo ? (
+            <BsPauseFill fontSize={30} />
+          ) : (
+            <BsPlayFill fontSize={30} />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
